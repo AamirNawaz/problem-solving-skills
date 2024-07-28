d2559b4e8f8004ae2761662a841238aa3eda2383
Aamirkhan <aamir.nawaz990@gmail.com>
2024-07-28 17:33:53 +0500

    Finding a prime number done
    core logic - a number which divisible by itself and by 1
    for coding - just divide num % 2 ==0 if reminder is zero means it's not a prime number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,29 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Check a number is prime or no ?");
+        System.out.println("/***************************************************/");
+        System.out.println("-------------  Prime Number Checker ---------------");
+        System.out.println("/***************************************************/");
+
+
+        //Solution
+        int number = 5;
+        boolean isPrime = isPrimNumber(number);
+        if(isPrime){
+            System.out.println(number + " is prime number!");
+        }else {
+            System.out.println(number + " is not a prime number!");
+        }
+
     }
+
+    //Prime Number method
+    public static boolean isPrimNumber(int number){
+        if(number %2 ==0){
+            return false;
+        }
+        return true;
+    }
+
+
 }
