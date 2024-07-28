c8b7b105d8c0846ad42018e49c7c445f7f9bffda
Aamirkhan <aamir.nawaz990@gmail.com>
2024-07-28 17:36:56 +0500

    Prime number move to Prime Number package

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,28 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("/***************************************************/");
-        System.out.println("-------------  Prime Number Checker ---------------");
+        System.out.println("-------------  Main function ---------------");
         System.out.println("/***************************************************/");
 
-
-        //Solution
-        int number = 5;
-        boolean isPrime = isPrimNumber(number);
-        if(isPrime){
-            System.out.println(number + " is prime number!");
-        }else {
-            System.out.println(number + " is not a prime number!");
-        }
-
     }
-
-    //Prime Number method
-    public static boolean isPrimNumber(int number){
-        if(number %2 ==0){
-            return false;
-        }
-        return true;
-    }
-
-
 }
---
+++ src/main/java/org/example/Prime_number/IsPrimeNumber.java
@@ -0,0 +1,30 @@
+package org.example.Prime_number;
+
+public class IsPrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("/***************************************************/");
+        System.out.println("-------------  Prime Number Checker ---------------");
+        System.out.println("/***************************************************/");
+
+
+        //Solution
+        int number = 5;
+        boolean isPrime = isPrimNumber(number);
+        if(isPrime){
+            System.out.println(number + " is prime number!");
+        }else {
+            System.out.println(number + " is not a prime number!");
+        }
+
+    }
+
+    //Prime Number method
+    public static boolean isPrimNumber(int number){
+        if(number %2 ==0){
+            return false;
+        }
+        return true;
+    }
+
+
+}
