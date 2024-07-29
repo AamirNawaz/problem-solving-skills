6e9d1a0432e4d90b1dd99f4792c0b1daf89b1188
Aamirkhan <aamir.nawaz990@gmail.com>
2024-07-29 14:15:08 +0500

    DSA - sorting array with bubble sort algorithm

---
+++ src/main/java/org/example/SortingWithDsa/BubbleSort.java
@@ -0,0 +1,23 @@
+package org.example.SortingWithDsa;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("*********** Bubble Sort ****************");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        int[] numbers = {6,9,7,3,8,5,2,1,4,10};
+        for (int i =0; i< numbers.length -1; i++){
+            for(int j =0; j <numbers.length -1-i; j++){
+                if(numbers[j] >= numbers[j+1]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
